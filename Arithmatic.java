cfe79cf76306090a23fdf1f8fe0f2bf57d340052
sonalishinde-dev <sonalishinde08123@gmail.com>
2025-09-04 13:00:38 +0530

    opreater commit

---
+++ Arithmatic.java
@@ -0,0 +1,15 @@
+package opreatre;
+
+public class Arithmatic {
+
+	public static void main(String[] args) {
+		
+		int num=10;
+		
+		System.out.println((num++)+10);
+		
+		System.out.println(num);
+
+	}
+
+}
---
+++ Incriment.java
@@ -0,0 +1,23 @@
+package opreatre;
+
+public class Incriment {
+
+	public static void main(String[] args) {
+		
+		int age=10;
+		
+	
+		
+		System.out.println(age+(++age)+(age++)+age+(age++));
+		
+		System.out.println(age);
+		
+		System.out.println(age+(age--)+(age++)+(--age)+(++age));
+		
+		 
+		
+		
+
+	}
+
+}
