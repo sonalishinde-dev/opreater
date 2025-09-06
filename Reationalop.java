f0ebd548d1d5d1cd97d37fce1e75a5e849852021
sonalishinde-dev <sonalishinde08123@gmail.com>
2025-09-06 21:01:30 +0530

    reational op

---
+++ Reationalop.java
@@ -0,0 +1,18 @@
+package opreatre;
+
+public class Reationalop {
+
+	public static void main(String[] args) {
+		
+		int a = 10;
+        int b = 5;
+        
+
+		  //Relational Operators
+        System.out.println("a == b: " + (a == b)); // Equal to
+        System.out.println("a > b: " + (a > b));   // Greater than
+        System.out.println("a=>b: "+ (a >= b));
+
+	}
+
+}
