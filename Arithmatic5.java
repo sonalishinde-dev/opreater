f458121da1def4bce8846752ed9fd5851227cf84
sonalishinde-dev <sonalishinde08123@gmail.com>
2025-09-08 11:21:25 +0530

    arithmatic op

---
+++ Arithmatic5.java
@@ -0,0 +1,26 @@
+package opreatre;
+
+public class Arithmatic5 {
+
+	public static void main(String[] args) {
+ 
+		int a=10;
+		float b=4.0f;
+		
+		System.out.println(a+b);
+		System.out.println(a-b);
+		System.out.println(a*b);
+        System.out.println(a/b);
+        System.out.println(a%b);
+        System.out.println(a==b);
+        
+        a+=5;
+   
+        System.out.println(a);
+        
+        
+        
+	
+	}
+
+}
