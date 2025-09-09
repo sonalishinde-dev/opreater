80f54820113b51a6673cd48185de871403fae50b
sonalishinde-dev <sonalishinde08123@gmail.com>
2025-09-09 15:48:31 +0530

    logical && opreater

--- Logical.java
+++ Logical.java
@@ -4,29 +4,14 @@ public class Logical {
 
 	public static void main(String[] args) {
 		
+		int a=35;
+		int b=40;
 		
-		int a = 10;
-        int b = 5;
-		int c = a;
+		System.out.println(a<b && b>a);
+		System.out.println(a<=b && a>=b);
 		
-		 //Assignment Operators
-       
-        System.out.println("c = " + c);
-        c += b; // Compound assignment (c = c + b)
-        System.out.println("c after += b: " + c); 
-        
-        System.out.println("c =" +b);
-        c -= b;
-        System.out.println("c befor -= b: " + c); 
-       
-        System.out.println("c =" +a);
-        c += a;
-        System.out.println("c = " +a);
-         
-        System.out.println("c =" +a);
-        c= -a;
-        System.out.println("c =" +b);
-        
+		System.out.println(18<30 && 40>=20);
+		
         
 
 
