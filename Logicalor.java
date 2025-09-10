4db0f64639f6d38e046d6adc72fac554147d6583
sonalishinde-dev <sonalishinde08123@gmail.com>
2025-09-10 11:25:59 +0530

    logical or opreation

---
+++ Logicalor.java
@@ -0,0 +1,16 @@
+package opreatre;
+
+public class Logicalor {
+
+	public static void main(String[] args) {
+
+		int num=10;
+		num++;
+		--num;
+		
+		System.out.println(num>=num || ++num<=num--);
+		
+		
+	}
+
+}
