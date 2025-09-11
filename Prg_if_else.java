8375c8a8bb1ccf4d43aee494c2b29893ce0c6c71
sonalishinde-dev <sonalishinde08123@gmail.com>
2025-09-11 17:37:09 +0530

    election program

---
+++ Prg_if_else.java
@@ -0,0 +1,20 @@
+package opreatre;
+
+public class Prg_if_else {
+
+	public static void main(String[] args) {
+		
+		int val=10;
+		int val1=20;
+		int val2=30;
+		if(val<=val2 && val<=val1 && val2<=val )
+		{
+			System.out.println(" print a value");
+		}
+		else
+		{
+			System.out.println("not print a value");
+		}
+	}
+
+}
